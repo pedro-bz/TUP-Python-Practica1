a115fd70476b5813903a1d7565ab19bc707c1841
pedro-bz <benzinapedro@gmail.com>
2022-08-10 11:25:05 -0300

    First exercise

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -9,7 +9,7 @@ Restricción: Usar el operador de multiplicación
 lado_cuadrado = 5
 
 # COMPLETAR - INICIO
-
+area_cuadrado = lado_cuadrado*lado_cuadrado
 # COMPLETAR - FIN
 
 assert area_cuadrado == 25
